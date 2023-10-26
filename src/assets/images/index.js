9cd6304e9c0ae7b4e87f55257d9b18be8536143a
Purbarun <purbarunmondal@gmail.com>
2023-10-26 11:12:04 +0530

    svg to png body background

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,5 @@
 import HeroImg from '../images/doctorHero.jpg';
-import BackGround from '../images/BackGround.svg';
+import BackGround from '../images/BackGround.png';
 import PatientDoc from '../images/patient-doc.jpg';
 import Service1 from '../images/service1.jpg';
 import Doc1 from '../images/doctor1.jpg';
